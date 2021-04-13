9fd44c166b054749e40350dd00191d8639c1ad8d
Akseli Keinänen <keinanenakseli@gmail.com>
2021-04-13 20:49:26 +0300

    Kehitetty visaa eteenpäin

--- akseli/js/scripts.js
+++ akseli/js/scripts.js
@@ -2,14 +2,36 @@ $(function() {
 
     let questionCount = 0;
 
-    // Button event
-    $("body").on("click", ".toggler", function() {
-        $(`#${$(this).attr("data-show")}`).show();
-        $(`#${$(this).attr("data-hide")}`).hide();
+    // Next page
+    function nextPage(e) {
+        $(`#${$(e).attr("data-show")}`).show();
+        $(`#${$(e).attr("data-hide")}`).hide();
 
         $("#questionCount").html(`Kysymys ${questionCount}`);
+    };
+
+    // Messages
+
+    function notification(type, content) {
+        $("#notification").html(`<div class="bg-${type}">${content}</div>`);
+    }
+
+    $(".toggler").on("click", function() {
+        nextPage(this)
     })
 
+    // Answer events
+
+    $("#answer-1").on("click", function() {
+        let answer = $("[name='question1']:checked").val();
+        if(answer === "2") {
+            nextPage(this)
+        } else if(answer === "1" || answer === "3") {
+            notification("warning", "VÄÄRÄ VASTAUS");
+        } else if(!answer) {
+            console.log("Mikäli napin painaminen tuottaa sinulle vaikeuksia, suosittelen seuraamaan tätä linkkiä")
+        }
+    });
 
 
 })
