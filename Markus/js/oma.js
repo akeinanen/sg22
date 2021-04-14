b0c0f1f3a7ecb9a90c5221f258c71fa32564f699
Markus Erkkilä <n0erma00@students.oamk.fi>
2021-04-14 19:16:28 +0300

    index sivun linkkaus

--- Markus/js/oma.js
+++ Markus/js/oma.js
@@ -1,9 +1,16 @@
 /// <reference path="jquery-3.6.0.min.js" /> 
 
-const visaIkkuna
 
-$(function(){
+$(function () {
 
-  
+    let selection = $("[#kysymys1]:checked").val();
+    let vastaus1 = "";
+    if (selection === 1) {
+        vastaus = true;
+    }
+    else {
+        vastaus = false;
+    }
+
 
 });
