50585cf2d9eb14184eda0bc54dc6702c47baa9f5
Akseli Keinänen <keinanenakseli@gmail.com>
2021-04-15 22:15:26 +0300

    Päivityksiä

--- akseli/js/scripts.js
+++ akseli/js/scripts.js
@@ -1,19 +1,34 @@
 $(function() {
 
     let questionCount = 0;
+    let triesLeft = 3;
 
     // Next page
     function nextPage(e) {
         $(`#${$(e).attr("data-show")}`).show();
         $(`#${$(e).attr("data-hide")}`).hide();
 
+        $("#notification").html("");
+
         $("#questionCount").html(`Kysymys ${questionCount}`);
+        $("#triesLeft").html(`Yritykset: ${triesLeft}`);
     };
+
+    function gameOver() {
+        triesLeft = 3;
+        clearGame();
+        $("#game-container").hide();
+        $("#gameOver").show();
+    }
+
+    function clearGame() {
+        $("#notification").html("");
+    }
 
     // Messages
 
     function notification(type, content) {
-        $("#notification").html(`<div class="bg-${type}">${content}</div>`);
+        $("#notification").html(`<div style="inline-block" class="bg-light border text-${type} px-3 py-1 mt-3">${content}</div>`);
     }
 
     $(".toggler").on("click", function() {
@@ -27,9 +42,14 @@ $(function() {
         if(answer === "2") {
             nextPage(this)
         } else if(answer === "1" || answer === "3") {
-            notification("warning", "VÄÄRÄ VASTAUS");
+            notification("danger", "Auts, vastasit väärin!");
+            triesLeft--;
+            $("#triesLeft").html(`Yritykset: ${triesLeft}`);
+            if(triesLeft < 1) {
+                gameOver();
+            }
         } else if(!answer) {
-            console.log("Mikäli napin painaminen tuottaa sinulle vaikeuksia, suosittelen seuraamaan tätä linkkiä")
+            notification("warning", "Mikäli valinnan vaikeus tuottaa sinulle ongelmia, suosittelen seuraamaan tätä linkkiä")
         }
     });
 
