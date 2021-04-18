b321be2fe47b7c95b71477dc3e0021c636d86634
Markus Erkkilä <n0erma00@students.oamk.fi>
2021-04-18 18:28:35 +0300

    visakoodit

--- Markus/js/oma.js
+++ Markus/js/oma.js
@@ -1,16 +1,46 @@
-/// <reference path="jquery-3.6.0.min.js" /> 
+/// <reference path="jquery-3.6.0.min.js" />
 
+// Markus Erkkilä
 
-$(function () {
+$(function() {
 
-    let selection = $("[#kysymys1]:checked").val();
-    let vastaus1 = "";
-    if (selection === 1) {
-        vastaus = true;
-    }
-    else {
-        vastaus = false;
-    }
+    
 
+    $("#vastaa").on("click", function () {  
+        let oikein = 0;
+        let vastaus1 = $("[name=vastaus1]:checked").val();
+        let vastaus2 = $("[name=vastaus2]:checked").val();
+        let vastaus3 = $("[name=vastaus3]:checked").val();
+        let vastaus4 = $("[name=vastaus4]:checked").val();
+        let vastaus5 = $("[name=vastaus5]:checked").val();
 
-});
+        if (vastaus1 === "1") {
+            oikein += 1;
+        }
+        if (vastaus2 === "1") {
+            oikein += 1;
+        }
+        if (vastaus3 === "1") {
+            oikein += 1;
+        }
+        if (vastaus4 === "1") {
+            oikein += 1;
+        }
+        if (vastaus5 === "1") {
+            oikein += 1;
+        }
+        
+        $("#tulos").html("Vastasit oikein " + oikein + " " + "/ 5 kysymyksestä");
+    })
+    
+    $("#alusta").on("click", function() {
+        $("#tulos").html("");
+        $(window).scrollTop(0);  
+        $('input[name=vastaus1]').prop('checked', false);
+        $('input[name=vastaus2]').prop('checked', false); 
+        $('input[name=vastaus3]').prop('checked', false); 
+        $('input[name=vastaus4]').prop('checked', false);
+        $('input[name=vastaus5]').prop('checked', false);    
+    });
+    
+})
