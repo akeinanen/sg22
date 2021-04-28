bb22cc32ad6db1258ec3d55b98ccd54e5a7730b9
Matleena Autio <c0auma00@students.oamk.fi>
2021-04-28 14:55:54 +0300

    Korttien muokkaus

--- Matleena/js/matikkavisa.js
+++ Matleena/js/matikkavisa.js
@@ -1,8 +1,10 @@
 /// <reference path="jquery-3.6.0.min.js" />
 
-// Matleena Autio
+// Matleena Autio, matikkavisa
 
 $(function(){
+
+    let oikeat_vastaukset = 0;
 
     $("#vastaa1").on("click", function(){
 
@@ -10,9 +12,14 @@ $(function(){
 
         if (valinta === "1") {
             $("#tulos1").html("Hienoa! Vastauksesi on oikein.");
+            oikeat_vastaukset = oikeat_vastaukset + 1;
+            $("#total").html(oikeat_vastaukset + " kysymykseen!");
         } else {
             $("#tulos1").html("Höh! Vastasit väärin.");
         }
+
+        $(".kysymys1").prop("disabled", true);
+
     });
 
     $("#vastaa2").on("click", function(){
@@ -21,9 +28,14 @@ $(function(){
 
         if (valinta === "1") {
             $("#tulos2").html("Jes! Vastasit oikein.");
+            oikeat_vastaukset = oikeat_vastaukset + 1;
+            $("#total").html(oikeat_vastaukset + " kysymykseen!");
         } else {
             $("#tulos2").html("Voi harmi! Väärin meni.");
         }
+
+        $(".kysymys2").prop("disabled", true);
+
     });
 
     $("#vastaa3").on("click", function(){
@@ -32,9 +44,14 @@ $(function(){
 
         if (valinta === "1") {
             $("#tulos3").html("Hyvä!");
+            oikeat_vastaukset = oikeat_vastaukset + 1;
+            $("#total").html(oikeat_vastaukset + " kysymykseen!");
         } else {
-            $("#tulos3").html(".");
+            $("#tulos3").html("Voi harmi. Nyt meni väärin.");
         }
+
+        $(".kysymys3").prop("disabled", true);
+
     });
 
     $("#vastaa4").on("click", function(){
@@ -43,9 +60,14 @@ $(function(){
 
         if (valinta === "1") {
             $("#tulos4").html("Hienoa! Oikein meni.");
+            oikeat_vastaukset = oikeat_vastaukset + 1;
+            $("#total").html(oikeat_vastaukset + " kysymykseen!");
         } else {
             $("#tulos4").html("Höh! Vastaus on väärin.");
         }
+
+        $(".kysymys4").prop("disabled", true);
+
     });
 
     $("#vastaa5").on("click", function(){
@@ -54,9 +76,30 @@ $(function(){
 
         if (valinta === "1") {
             $("#tulos5").html("Jes! Vastaus on oikein.");
+            oikeat_vastaukset = oikeat_vastaukset + 1;
+            $("#total").html(oikeat_vastaukset + " kysymykseen!");
         } else {
             $("#tulos5").html("Höh! Nyt meni mönkään.");
         }
+
+        $(".kysymys5").prop("disabled", true);
+
+        let palkinto1 = $("#prize1").val();
+        let palkinto2 = $("#prize2").val();
+        let palkinto3 = $("#prize3").val();
+
+        if (oikeat_vastaukset === 5) {
+            $("#prize").removeClass("hided");
+            $("#prize").html(palkinto1);
+            
+        } else if (oikeat_vastaukset >= 3) {
+            $("#prize").removeClass("hided");
+            $("#prize").html(palkinto2);
+        } else {
+            $("#prize").removeClass("hided");
+            $("#prize").html(palkinto3);
+        }
+
     });
 
 });
