614e08828426d63cc04c8634c71945f6d966cb6a
Akseli Keinänen <keinanenakseli@gmail.com>
2021-05-02 10:33:06 +0300

    Satunnaisia muutoksia

--- akseli/js/scripts.js
+++ akseli/js/scripts.js
@@ -37,7 +37,35 @@ $(function() {
 
     // Answer events
 
+    function answer(id, rightAnswer) {
+        let answer = $(`[name='question${id}']:checked`).val();
+        if(!answer) {
+            notification("warning", "Mikäli valinnan vaikeus tuottaa sinulle ongelmia, suosittelen vaihtamaa visaa");
+        } else if(answer === rightAnswer) {
+            nextPage($(`#answer-${id.toString()}`));
+        } else {
+            notification("danger", "Auts, vastasit väärin!");
+            triesLeft--;
+            $("#triesLeft").html(`Yritykset: ${triesLeft}`);
+            if(triesLeft < 1) {
+                gameOver();
+            }
+        }
+    }
+
     $("#answer-1").on("click", function() {
+        answer(1, "2");
+    })
+
+    $("#answer-2").on("click", function() {
+        answer(2, "2");
+    })
+
+    $("#answer-3").on("click", function() {
+        answer(2, "3");
+    })
+
+   /* $("#answer-1").on("click", function() {
         let answer = $("[name='question1']:checked").val();
         if(answer === "2") {
             nextPage(this)
@@ -52,6 +80,9 @@ $(function() {
             notification("warning", "Mikäli valinnan vaikeus tuottaa sinulle ongelmia, suosittelen seuraamaan tätä linkkiä")
         }
     });
-
-
+    $("#answer-2").on("click", function() {
+        let answer = $("[name='question1']:checked").val();
+    */ 
+   
+    
 })
